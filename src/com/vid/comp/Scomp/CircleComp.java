c774e37e1461f0d7b350b204f2c84e95325ea5de
parabpratham <parabpratham@gmail.com>
2016-07-04 10:15:41 +0530

    4 july 2016

---
+++ src/com/vid/comp/Scomp/CircleComp.java
@@ -0,0 +1,76 @@
+package com.vid.comp.Scomp;
+
+import java.net.URL;
+
+import com.vid.comp.Jcomp.AbstractComp;
+import com.vid.overlay.comp.master.SHAPE_TYPE;
+
+import javafx.scene.image.Image;
+
+public class CircleComp extends AbstractComp {
+
+	private double radius;
+	private double centerX;
+	private double centerY;
+
+	public double getCenterX() {
+		return centerX;
+	}
+
+	public void setCenterX(double centerX) {
+		this.centerX = centerX;
+	}
+
+	public double getCenterY() {
+		return centerY;
+	}
+
+	public void setCenterY(double centerY) {
+		this.centerY = centerY;
+	}
+
+	public double getRadius() {
+		return radius;
+	}
+
+	public void setRadius(double radius) {
+		this.radius = radius;
+	}
+
+	public CircleComp() {
+		setTextIncluded(false);
+		setBgImageOption(false);
+		setInfopanel(true);
+		setAnn_type(CircleComp.class.getName());
+		setAnnName("Circle");
+		setControllerClass("com.vid.controller.comp.CircleAddController");
+		setFXMLPath("fxml/addcompcont/Circle_add_popup.fxml");
+		setShapeType(SHAPE_TYPE.CIRCLE);
+		
+	}
+
+	@Override
+	public Image getGraphic() {
+		URL resource = getClass().getClassLoader().getResource("icons/circle95.png");
+		Image image = new Image("file:" + resource.getPath());
+		return image;
+	}
+
+	@Override
+	public String toXml() {
+		String annot = "<annotation id=\"" + getId() + "\" \n type=\"" + getAnn_type() + "\">\n" + "<starttime>"
+				+ getStartTime() + "</starttime>\n";
+		annot += "<endtime> " + getEndTime() + "</endtime>";
+		annot += "<comp_type>SCOMPONENT</comp_type>";
+		annot += "<parameters set=\"" + 1 + "\"> \n";
+		annot += "<CenterX> " + getCenterX() + "</CenterX> \n";
+		annot += "<CenterY>" + getCenterY() + "</CenterY> \n";
+		annot += "<Radius>" + getRadius() + "</Radius> \n";
+		annot += "<BgColor>" + getBgColor() + "</BgColor> \n";
+		annot += "<FillShape>" + isFillShape() + "</FillShape> \n";
+		annot += "</parameters> \n";
+		annot += "</annotation> \n";
+		return annot;
+	}
+
+}
